884b8e1522345499bd458200ef17129868ff2947
DAO <daohdde170519@fpt.edu.vn>
2023-09-27 11:10:52 +0700

    First Commit

---
+++ src/main/java/manager/Main.java
@@ -0,0 +1,52 @@
+package manager;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            normalizeText("src/main/java/manager/Input.txt", "src/main/java/manager/Output.txt");
+        } catch (IOException e) {
+           
+
+        }
+    }
+
+    public static void normalizeText(String inputFileName, String outputFileName) throws IOException {
+        try ( BufferedReader reader = new BufferedReader(new FileReader("src/main/java/manager/Input.txt"));  BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/manager/Output.txt"))) {
+
+            String line;
+            boolean firstLine = true;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim().replaceAll("\\s+", " ");
+
+                if (!line.isEmpty()) {
+                    if (firstLine) {
+                        line = line.substring(0, 1).toUpperCase() + line.substring(1);
+                        firstLine = false;
+                    }
+
+                    line = line.replaceAll("([,.:])\\s*", "$1 ");
+                    line = line.replaceAll("\\s([,.])", "$1");
+                    line = line.replaceAll("([,.])\\s(\\p{Lower})", "$1 $2");
+                    line = line.replaceAll("\"\\s*([^\"\\s]+)\\s*\"", "\"$1\"");
+                    writer.write(line);
+//                    writer.newLine();
+                }
+                
+            }
+
+                writer.write(".");
+            
+           
+
+        } catch (IOException e) {
+            throw new IOException("An error occurred while processing the file.", e);
+        }
+    }
+}
